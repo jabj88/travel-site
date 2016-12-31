b3384309b63360f401711d5d41b991f4effd4557
Jorge <jabj88@gmail.com>
2016-12-31 07:59:20 -0500

    Reveal on scroll done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
-
 import MobileMenu from './modules/MobileMenu'
-
+import RevealOnScroll from './modules/RevealOnScroll'
+import $ from 'jquery'
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonials"), "70%");
